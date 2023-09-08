ea8bb18043b35f8600918d826f6b8d87c43d4cba
mispranami <pranamiswarajindia@gmail.com>
2023-09-09 00:19:12 +0530

    Add files via upload

---
+++ as1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+   string name;
+   string clgname;
+   int age;
+   cout<<"enter your name \n";
+   cin>>name;
+   cout<<"enter your age \n ";
+   cin>>age;
+   cout<<"enter your college name \n";
+   cin>>clgname;
+   cout<<"hello "<<name<<", i see you are "<<age<<" years old and are currently studying at \n"<<clgname;
+
+return 0;
+}
