53fec6a7b35db6152d6d83789c9c629bab3a8fe8
mispranami <pranamiswarajindia@gmail.com>
2023-10-09 22:48:53 +0530

    Add files via upload

---
+++ ass2.2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+double r,area;
+const double pi=3.14;
+cout<<"enter the radius of circle "<<endl;
+cin>>r;
+area=pi*r*r;
+cout<<"the area of circle is "<<area<<endl;
+return 0;
+
+
+ }
