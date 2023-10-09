1a10095509d25b9e5a05babe936fdfd85d2f8791
mispranami <pranamiswarajindia@gmail.com>
2023-10-09 22:49:13 +0530

    Add files via upload

---
+++ ass2.4.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter your age "<<endl;
+    cin>>a;
+    if (a>=60)
+    {
+         cout<<"user is senior citizen "<<endl;
+    }
+      else 
+      
+       if(a>=18)
+    {
+        cout<<"user is adult "<<endl;
+
+    }
+    else if (a>=13)
+    {
+        cout<<"user is teenager"<<endl;
+    }
+    else
+    {
+        cout<<"user is child "<<endl;
+    }
+return 0;
+}
+    
+
+
+
+
+    
+    
