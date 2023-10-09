f1bfb7f3d768bcbba5b35079405c2ecf0f0b5e0d
mispranami <pranamiswarajindia@gmail.com>
2023-10-09 22:41:02 +0530

    Add files via upload

---
+++ ass2.1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a;
+    
+    cout<<"enter a floating value "<<endl;
+    cin>>a;
+    cout<<"the floating value is "<<a<<endl;
+    cout<<"the integer value is "<<int(a)<<endl;
+    return 0;
+}
+ 
+
+
+
+
+
+    
+
